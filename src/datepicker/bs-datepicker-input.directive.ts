0ca0e8fc45f320fe69c0127404d3efe5cf473970
hugoj-goncalves <hugo159@gmail.com>
2024-09-11 16:20:31 -0300

    fix

--- src/datepicker/bs-datepicker-input.directive.ts
+++ src/datepicker/bs-datepicker-input.directive.ts
@@ -152,13 +152,17 @@ export class BsDatepickerInputDirective implements ControlValueAccessor, Validat
       }
 
       if (this._picker && this._picker.minDate && isBefore(_value, this._picker.minDate, 'date')) {
-        this.writeValue(this._picker.minDate);
+        if (!this._picker._config.keepDatesOutOfRules) {
+          this.writeValue(this._picker.minDate);
+        }
 
         return { bsDate: { minDate: this._picker.minDate } };
       }
 
       if (this._picker && this._picker.maxDate && isAfter(_value, this._picker.maxDate, 'date')) {
-        this.writeValue(this._picker.maxDate);
+        if (!this._picker._config.keepDatesOutOfRules) {
+          this.writeValue(this._picker.maxDate);
+        }
 
         return { bsDate: { maxDate: this._picker.maxDate } };
       }
